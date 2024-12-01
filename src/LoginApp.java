777d3afbd8264ebcb0d5e70b9be20fb1e0356032
Qaisar Mateen <qaisarmateen313@gmail.com>
2024-12-01 23:49:55 +0500

    Update LoginApp.java

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -47,7 +47,7 @@ public class LoginApp extends JFrame {
             String email = emailField.getText();
             String password = new String(passwordField.getPassword()); // Password is ignored for validation
 
-            String userName = authenticateUser(email);
+            String userName = authenticateUser(email, password);
             if (userName != null) {
                 JOptionPane.showMessageDialog(null, "Welcome, " + userName + "!", "Login Successful", JOptionPane.INFORMATION_MESSAGE);
             } else {
@@ -56,7 +56,7 @@ public class LoginApp extends JFrame {
         }
     }
 
-    String authenticateUser(String email) {
+    String authenticateUser(String email, String password) {
         String userName = null;
         try (Connection conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD)) {
             String query = "SELECT name FROM User WHERE Email = ?";
