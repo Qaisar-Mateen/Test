aa2f1ed312ff63c200308df4e550b5dc00345513
Qaisar Mateen <qaisarmateen313@gmail.com>
2024-12-02 17:56:58 +0500

    Update LoginApp.java

--- src/LoginApp.java
+++ src/LoginApp.java
@@ -78,7 +78,6 @@ public class LoginApp extends JFrame {
     public static void main(String[] args) {
         SwingUtilities.invokeLater(() -> {
             LoginApp loginApp = new LoginApp();
-            loginApp.setVisible(false);
         });
     }
 
